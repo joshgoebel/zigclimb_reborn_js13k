11646cdb00954165c6d965e1588d2fba2d3e9e10
Josh Goebel <me@joshgoebel.com>
2020-08-20 11:04:05 -0400

    wip

--- src/ui/tile_render.js
+++ src/ui/tile_render.js
@@ -3,34 +3,64 @@ import { ClassicRender } from "./classic_render"
 
 import CSSGrid from "../lib/css_grid"
 
-TILES = {
+const MONSTER = 52
+const TILES = {
   "@": 6,
   "#": 13,
-  ".": 0
+  ".": 0,
+  "%": 21,
+  "(": 20,
+  "!": 35,
+  "<": 18,
+
+  // 16 monsters
+  "a": MONSTER+1, // brown bat
+  "b": MONSTER+7, // green thingy
+  "c": MONSTER+4,  // ostrich?
+  "d": MONSTER, // blue thingy?
+  "e": MONSTER + 18, // green snake
+  "f": MONSTER + 16, // grey horse?
+  "g": 5,
+  "h": 5,
+  "i": 5,
+  "j": MONSTER + 17, // brown snake
+  "k": MONSTER + 9, // gold dragon thing
+  "l": 5,
+  "m": 5,
+  "n": MONSTER + 3, // green dragon
+  "o": MONSTER + 15, // purple outline
+  "p": MONSTER + 22 // reaper/haunt in purple robe
 }
 
 class SpriteSheet {
   constructor(url, opts) {
     this.url = url
     this.opts = opts
+    this.renderSize = 32
+
+    let grid = document.querySelector("#grid")
+    grid.style.gridAutoColumns=`${this.renderSize}px`
   }
   render(cell, icon) {
     let tile = TILES[icon]
-    if (!tile) return;
+    if (tile === undefined) return;
+
+    cell.title = icon
+    cell.innerHTML = ""
+    cell.style.width=`${this.renderSize}px`
+    cell.style.height=`${this.renderSize}px`
 
     cell.style.backgroundImage = this.url;
     // TODO: move to css
     cell.style.imageRendering = "pixelated";
-    cell.style.backgroundSize = `auto calc(${this.opts.sheet[1]}px*40/16)`
-
+    cell.style.backgroundSize = `auto calc(${this.opts.sheet[1]}px*${this.renderSize}/16)`
 
     let tx = tile % this.opts.width
     let ty = Math.floor(tile / this.opts.width)
 
-    let x = tx * this.tile[0]
-    let y = ty * this.tile[1] + this.opts.yOffset
-
-    cell.style.backgroundPosition=`calc(${x}*40/16) calc(-${y}*40/16)`;
+    let x = tx * this.opts.tile[0]
+    let y = ty * this.opts.tile[1] + this.opts.yOffset
+    cell.style.backgroundPosition=`calc(-${x}px*${this.renderSize}/16) calc(-${y}px*${this.renderSize}/16)`;
   }
 }
 
@@ -46,9 +76,9 @@ const renderTile = (cell, icon) => {
   cell.innerHTML = icon
   cell.style.color=""
   cell.className = ""
-  cell.style.display="block"
-  cell.style.backgroundImage ="url(loveable_rogue.png)";
-  cell.style.backgroundPosition="0 0";
+  // cell.style.display="block"
+  // cell.style.backgroundImage ="url(loveable_rogue.png)";
+  // cell.style.backgroundPosition="0 0";
 
   if (icon == "~") {
     cell.style.backgroundColor = "#012"
@@ -57,7 +87,7 @@ const renderTile = (cell, icon) => {
   if (icon == ".") {
     // cell.style.backgroundColor = "#012"
     cell.style.color = "#333"
-    cell.className = "empty"
+    // cell.className = "empty"
   }
   if (icon >= "a" && icon <="z") {
     cell.style.color = "#fc6"
@@ -89,31 +119,31 @@ const renderTile = (cell, icon) => {
   // let child = document.createElement("div")
   // cell.append(child)
   // cell.style.width="40px"
-  cell.style.width="40px"
-  cell.style.height="40px"
-  cell.style.imageRendering="pixelated";
-  cell.style.backgroundSize="auto calc(240px*40/16)"
+  // cell.style.imageRendering="pixelated";
+  // cell.style.backgroundSize="auto calc(240px*40/16)"
   // cell.style.display="none"
 
-  if (icon == "@") {
-    // cell.cellNodes[0].remove()
-    cell.style.backgroundImage ="url(loveable_rogue.png)";
-    cell.style.backgroundPosition="calc(112px*40/16) calc(-144px*40/16)";
+  SS.render(cell, icon)
 
-  }
-  if (icon == "#") {
-    // console.log(cell.innerHTML)
-    // cell.cellNodes[0].remove()
-    cell.innerHTML=""
-    cell.style.backgroundImage ="url(loveable_rogue.png)";
-    cell.style.backgroundPosition="0px calc(-160px*40/16)";
-    // cell.style.backgroundColor="red";
-    // cell.style.border="2px solid red"
-    // cell.style.backgroundRepeat="no-repeat"
-    // cell.style.transform ="scaleX(2.7)"
-    // cell.style.color = "#666"
-    // cell.style.opacity="0.5"
-  }
+  // if (icon == "@") {
+  //   // cell.cellNodes[0].remove()
+  //   cell.style.backgroundImage ="url(loveable_rogue.png)";
+  //   cell.style.backgroundPosition="calc(112px*40/16) calc(-144px*40/16)";
+
+  // }
+  // if (icon == "#") {
+  //   // console.log(cell.innerHTML)
+  //   // cell.cellNodes[0].remove()
+  //   cell.innerHTML=""
+  //   cell.style.backgroundImage ="url(loveable_rogue.png)";
+  //   cell.style.backgroundPosition="0px calc(-160px*40/16)";
+  //   // cell.style.backgroundColor="red";
+  //   // cell.style.border="2px solid red"
+  //   // cell.style.backgroundRepeat="no-repeat"
+  //   // cell.style.transform ="scaleX(2.7)"
+  //   // cell.style.color = "#666"
+  //   // cell.style.opacity="0.5"
+  // }
 }
 
 class TileRender {
